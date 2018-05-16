46617b9b178315f295f5a5d45543c6d3ce578f50
Kosmas Chatzimichalis <kosmas@mach7x.com>
2018-05-16 20:00:27 +0200

    Add GraphiQL

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -13,6 +13,7 @@ defmodule PlateSlateWeb.Schema do
 
   query do
 
+    @desc "The list of available items on the menu"
     field :menu_items, list_of(:menu_item) do
       resolve fn _,_,_ ->
         {:ok, Repo.all(Menu.Item)}
