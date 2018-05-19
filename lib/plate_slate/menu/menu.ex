ba3a929a68351586956079d0c4fced525fd424a9
Kosmas Chatzimichalis <kosmas@mach7x.com>
2018-05-19 17:57:24 +0200

    Creating Custom Scalar Types

--- lib/plate_slate/menu/menu.ex
+++ lib/plate_slate/menu/menu.ex
@@ -122,6 +122,7 @@ defmodule PlateSlate.Menu do
 
   """
   def list_items(args) do
+    IO.puts "These are our arguments: #{inspect(args)}"
     args
     |> Enum.reduce(Item, fn
       {:order, order}, query ->
@@ -140,6 +141,10 @@ defmodule PlateSlate.Menu do
         from q in query, where: q.price >= ^price
       {:priced_below, price}, query ->
         from q in query, where: q.price <= ^price
+      {:added_after, date}, query ->
+        from q in query, where: q.added_on >= ^date
+      {:added_before, date}, query ->
+        from q in query, where: q.added_on <= ^date
       {:category, category_name}, query ->
         from q in query,
           join: c in assoc(q, :category),
