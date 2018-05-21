d6ee50662a050ec598b1fbbadb8adfb9d2658aec
Kosmas Chatzimichalis <kosmas@mach7x.com>
2018-05-21 15:47:17 +0200

    Organizing the Schema

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -11,6 +11,8 @@ defmodule PlateSlateWeb.Schema do
 
   alias PlateSlateWeb.Resolvers
 
+  import_types __MODULE__.MenuTypes
+
   query do
 
     @desc "The list of available items on the menu"
@@ -43,36 +45,5 @@ defmodule PlateSlateWeb.Schema do
     value :desc
   end
 
-  @desc "Filtering options for the menu item list"
-  input_object :menu_item_filter do
-
-    @desc "Matching a name"
-    field :name, :string
-
-    @desc "Matching a category name"
-    field :category, :string
-
-    @desc "Matching a tag"
-    field :tag, :string
-
-    @desc "Priced above a value"
-    field :priced_above, :float
-
-    @desc "Priced below a value"
-    field :priced_below, :float
-
-    @desc "Added to the menu before this date"
-    field :added_before, :date
-
-    @desc "Added to the menu after this date"
-    field :added_after, :date
-
-  end
-
-  object :menu_item do
-    field :id, :id
-    field :name, :string
-    field :description, :string
-    field :added_on, :date
-  end
+  # Common types; :data, :sort_order, etc
 end
